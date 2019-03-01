fb43100f49656558d19f88ac5490ad23cae9ea6e
rogozhuk <resmedia@ya.ru>
2019-03-01 17:34:17 +0300

    #Task_3 - Product special block

--- main.js
+++ main.js
@@ -18,7 +18,7 @@ for (i = 0; i < x.length; i++) {
         c.innerHTML = selElmnt.options[j].innerHTML;
         /*give him class with option class if not null*/
         c.setAttribute('class', 'select__option ' + (selElmnt.options[j].className ? selElmnt.options[j].className : ''));
-        c.addEventListener("click", function(e) {
+        c.addEventListener("click", function (e) {
             /*when an item is clicked, update the original select box,
             and the selected item:*/
             var i, s, h;
@@ -39,7 +39,7 @@ for (i = 0; i < x.length; i++) {
         b.appendChild(c);
     }
     x[i].appendChild(b);
-    a.addEventListener("click", function(e) {
+    a.addEventListener("click", function (e) {
         /*when the select box is clicked, close any other select boxes,
         and open/close the current select box:*/
         e.stopPropagation();
@@ -48,7 +48,9 @@ for (i = 0; i < x.length; i++) {
         this.classList.toggle("select-active");
     });
 }
+
 function closeAllSelect(elmnt) {
+    console.log(elmnt);
     /*a function that will close all select boxes in the document,
     except the current select box:*/
     var x, y, i, arrNo = [];
@@ -57,7 +59,7 @@ function closeAllSelect(elmnt) {
 
     for (i = 0; i < y.length; i++) {
         if (elmnt === y[i]) {
-            arrNo.push(y[i])
+            arrNo.push(i)
         } else {
             y[i].classList.remove("select-active");
         }
@@ -68,6 +70,7 @@ function closeAllSelect(elmnt) {
         }
     }
 }
+
 /*if the user clicks anywhere outside the select box,
 then close all select boxes:*/
 document.addEventListener("click", closeAllSelect);
@@ -79,14 +82,18 @@ function triggerMenu(idDropdown) {
 }
 
 //range
-function getVals(){
+function getValues() {
     // Get slider values
     var parent = this.parentNode;
     var slides = parent.getElementsByClassName('range-slider__input');
-    var slide1 = parseFloat( slides[0].value );
-    var slide2 = parseFloat( slides[1].value );
+    var slide1 = parseFloat(slides[0].value);
+    var slide2 = parseFloat(slides[1].value);
     // Neither slider will clip the other, so make sure we determine which is larger
-    if( slide1 > slide2 ){ var tmp = slide2; slide2 = slide1; slide1 = tmp; }
+    if (slide1 > slide2) {
+        var tmp = slide2;
+        slide2 = slide1;
+        slide1 = tmp;
+    }
 
     var displayElement1 = parent.getElementsByClassName("range-slider__max")[0];
     var displayElement2 = parent.getElementsByClassName("range-slider__min")[0];
@@ -94,17 +101,17 @@ function getVals(){
     displayElement1.innerHTML = "$ " + slide2;
 }
 
-window.onload = function(){
+window.onload = function () {
     // Initialize Sliders
     var sliderSections = document.getElementsByClassName("range-slider");
-    for( var x = 0; x < sliderSections.length; x++ ){
+    for (var x = 0; x < sliderSections.length; x++) {
         var sliders = sliderSections[x].getElementsByClassName('range-slider__input');
-        for( var y = 0; y < sliders.length; y++ ){
-            if( sliders[y].type ==="range" ){
-                sliders[y].oninput = getVals;
+        for (var y = 0; y < sliders.length; y++) {
+            if (sliders[y].type === "range") {
+                sliders[y].oninput = getValues;
                 // Manually trigger event first time to display values
                 sliders[y].oninput();
             }
         }
     }
-}
+};
